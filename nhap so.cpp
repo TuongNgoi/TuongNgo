9a1e69fcf7dbdfd30bfc1732336b0fc0312e508d
TuongNgoi <146446165+TuongNgoi@users.noreply.github.com>
2023-09-30 10:25:20 +0700

    Add files via upload

---
+++ nhap so.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using   namespace std;
+
+int main()
+{
+    cout <<"Nhap so 1: " << endl;
+    cout <<"Nhap so 2: " << endl;
+    cout <<"Nhap so 3: " << endl;
+    cout <<"Nhap so 4: " << endl;
+    cout <<"Nhap so 5: " << endl;
+    cout <<"Nhap so 6: " << endl;
+    cout <<"Nhap so 7: " << endl;   
+}
